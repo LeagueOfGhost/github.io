17f4932a1d8eebef50ccd63598c9d49044d50722
LeagueOfGhost <51259421+LeagueOfGhost@users.noreply.github.com>
2019-06-02 10:45:20 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let text = document.getElementById('text'),
-    enter = document.getElementById('enter'),
-    audio = document.getElementById('audio');
-    enter.addEventListener('click', ()=>{
-        audio.setAttribute('src', `${text.value}`);
-    });
-
